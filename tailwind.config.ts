0d6855df1f90819ced4d76a9325ee8799758e3ae
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-03-16 23:09:27 -0700

    Add DaisyUI themes and Tailwind Typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+            },
+        },
     },
-  },
-  plugins: [
-    require("daisyui"),
-  ],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    ...require('daisyui/src/theming/themes')['light'],
+                    primary: '#6174dd',
+                },
+            },
+            {
+                dark: {
+                    ...require('daisyui/src/theming/themes')['dark'],
+                    primary: '#6174dd',
+                },
+            },
+            'dracula',
+        ],
+    },
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
 export default config;
