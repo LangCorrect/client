e64dc3dacfc0a7c8dd2f47c1fc0da43c47d70418
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-03-27 06:00:53 -0700

    feat(Favicons): Add favicon and app icons
    Fix #11

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,26 @@ export const metadata: Metadata = {
     title: 'LangCorrect',
     description:
         "Master grammar, spelling, and syntax in the language(s) you're learning through direct feedback on your writing from fluent, native speakers.",
+    icons: [
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            url: '/favicon-32x32.png',
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            url: '/favicon-16x16.png',
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            url: '/apple-touch-icon.png',
+        },
+    ],
+    manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
